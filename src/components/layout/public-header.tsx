4ca47cc26f0d6dcac87638523c3ab3405aba6be3
prasads6507 <prasads6507@gmail.com>
2026-05-15 13:38:14 -0400

    style: DRASTICALLY enlarge logos for maximum visibility

--- src/components/layout/public-header.tsx
+++ src/components/layout/public-header.tsx
@@ -40,9 +40,9 @@ export function PublicHeader() {
           <Image 
             src="/logo.png" 
             alt="OrbitPOS Logo" 
-            width={180} 
-            height={60} 
-            className="h-10 w-auto transition-transform group-hover:scale-105" 
+            width={300} 
+            height={100} 
+            className="h-16 w-auto transition-transform group-hover:scale-105" 
           />
         </Link>
         
